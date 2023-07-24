aee47876990e8d487388cc5cb1b7994737a7ac62
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-07-24 20:40:56 +0200

    creating custom hook

--- src/components/Tasks/TaskItem.js
+++ src/components/Tasks/TaskItem.js
@@ -2,6 +2,5 @@ import classes from './TaskItem.module.css';
 
 const TaskItem = (props) => {
   return <li className={classes.task}>{props.children}</li>
-};
-
+}; 
 export default TaskItem;
---
+++ src/components/hooks/use-http.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const useHttp = () =>{
+  return (
+ 
+  )
+}
+
+export default useHttp;
